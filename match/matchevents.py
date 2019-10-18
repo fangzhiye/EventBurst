d65abf721ea11d0baab05eb16ec6b9e9d70f4d91
fangzhiye <929329108@qq.com>
2019-10-18 21:20:05 +0800

    add part match algorithm

--- match/matchevents.py
+++ match/matchevents.py
@@ -1,3 +1,4 @@
+#%%
 from scipy.optimize import linear_sum_assignment
 import numpy as np
 '''
@@ -6,9 +7,45 @@ If it has more rows than columns, then not every row needs to be assigned to a c
 #即如果行多则列的每个顶点都会匹配，如果列多则行的每个顶点都会匹配
 #我们需要的是最大权值和匹配
 '''
+#%%
+#是一array array里的元素是[{},{},{}]
+#{"community_id":社区划分后的id,"community_member":[社区成员],"member_degree":[成员的度数],"member_emb":[成员的embedding],"member_content":["社区内容"]}
+left_frame = np.load("./14.npy", allow_pickle=True)
+right_frame = np.load("./15.npy",allow_pickle=True)
+def get_commemb(frame):
+    embs = []
+    for comm in frame:
+        degree = np.array(comm['member_degree'])
+        emb = comm['member_emb']
+        all_degree = sum(degree)
+        degree = degree / all_degree
+        w = np.expand_dims(degree,axis = 1)
+        emb *= w
+        embs.append(np.sum(emb,axis=0))
+    return np.array(embs)
+left_embs = get_commemb(left_frame)
+right_embs = get_commemb(right_frame)
+l = len(left_frame)
+r = len(right_frame)
+print("left frame communities : {}".format(l))
+print("right frame communities : {}".format(r))
+sim = np.matmul(left_embs,right_embs.transpose())
+print(sim.shape)
+#%%
+
+#构早相似度矩阵
 #cost = np.array([[4.5, 1, 3], [2, 0, 5], [3, 2, 2],[9,0,1]]) * -1#
-cost = np.array([[1,2,3],[3,4,5]])
-row_ind, col_ind = linear_sum_assignment(cost)
+row_ind, col_ind = linear_sum_assignment(sim*-1)
 print(row_ind)
 print(col_ind)
-print(cost[row_ind, col_ind].sum()*-1)#因原算法是最小权值匹配所以要乘-1
+print(sim[row_ind, col_ind].sum())#因原算法是最小权值匹配所以要乘-1
+match_left = left_frame[row_ind]
+match_right = right_frame[col_ind]
+for i in range(len(row_ind)):
+    print("left 的举报数目是:{},内容是:".format(len(match_left[i]["member_degree"])))
+    print(match_left[i]["member_content"])
+    print("匹配的是：")
+    print("right 的举报数目是:{},内容是:".format(len(match_right[i]["member_degree"])))
+    print(match_right[i]["member_content"])
+    print("<--------------------------------------------->")
+#%%
--- mycommunity/community_detection.py
+++ mycommunity/community_detection.py
@@ -12,6 +12,8 @@ import datetime
 from community import community_louvain
 from collections import defaultdict
 import numpy as np
+from sklearn.feature_extraction.text import CountVectorizer 
+from sklearn.feature_extraction.text import TfidfTransformer
 #为构图准备数据集
 #%%
 project_path = "D:/EVENTBURST/data"
@@ -47,8 +49,8 @@ def timestamp2time(timestamp):
     dt = time.strftime("%Y-%m-%d %H:%M:%S",time_local)
     return dt
 #%%
-time_b = '2015/11/15 00:00:00'
-time_e = '2015/11/15 23:00:00'
+time_b = '2015/11/10 00:00:00'
+time_e = '2015/11/10 23:59:59'
 print("查询数据的起始时间是:{},查询的结速时间是:{}".format(time_b,time_e))
 q_b = time2timestamp(time_b)
 q_e = time2timestamp(time_e)
@@ -57,6 +59,19 @@ ret = ret.reset_index(drop = True)
 m,n = ret.shape
 print("查询数据的行数为:{},查询数据的列数为:{}".format(m,n))
 keywords = ret['KEY_WORDS']
+#%%
+#tf-idf的权值
+corpus = list(keywords)
+vectorizer = CountVectorizer()  
+X = vectorizer.fit_transform(corpus)  
+word = vectorizer.get_feature_names()  
+transformer = TfidfTransformer() 
+tfidf = transformer.fit_transform(X).toarray()#tfidf[i][j]是表示第i个文档下标为j的词的权重，所有词都有一个下标
+#for i in range(10):
+tfidf_word2id ={}
+for i,word in enumerate(word):
+    tfidf_word2id[word] = i#{词:id}
+#%%
 ret['EMBEDDINGS'] = None
 emb_temp = []
 for i in range(m):
@@ -65,7 +80,8 @@ for i in range(m):
     n = 0
     for k in ks:
         try:
-            emb = final_embeddings[words2idx[k]]#如果是未登陆词#emb为0
+            #emb = final_embeddings[words2idx[k]]*tfidf[i][tfidf_word2id[k]]#如果是未登陆词#emb为0,使用tfidf加权计算emb的值
+            emb = final_embeddings[words2idx[k]]#不使用tf-idf,而是ebm的均值
             n+=1
         except:
             emb = np.zeros(128)
@@ -76,16 +92,18 @@ for i in range(m):
         ksemb/=n#每条举报数据的embding是所有关键emb的均值
     emb_temp.append(ksemb)
 ret['EMBEDDINGS'] = emb_temp
-
+emb_temp = np.array(emb_temp)
 #%%
 #构建图的结点集合
 nodes = []
 for i in range(m):
     nodes.append((i,{'key_words':keywords[i]}))
 #构建边的集合
-sim = np.matmul(np.array(emb_temp),np.array(emb_temp).transpose())#计算文档与文档间的相似矩阵
+sim = np.matmul(emb_temp,emb_temp.transpose())#计算文档与文档间的相似矩阵
 print(sim.shape)
-T = 0.4#相似度的阈值
+#T值越大划分的社区越多,社区内的类别越细
+T = 0.4#相似度的阈值,加了tf_idf权重后,相似性大于T的边减少了,阈值越大则边的数目越少,实验发现T大社区数目增多
+print("边相似性的阈值是:{}".format(T))
 edges = []
 for i in range(m):
     for j in range(i+1,m):
@@ -95,6 +113,9 @@ for i in range(m):
 G = nx.Graph()
 G.add_edges_from(edges)
 G.add_nodes_from(nodes)
+print("图的结点数:{}".format(G.number_of_nodes()))
+print("图的边数:{}".format(G.number_of_edges()))
+#%%
 #nodes = [(1,{'color':'red'}),(2,{'color':'blue'}),(3,{'color':'black'}),(4,{'color':'white'})]#增加结点和结点属性
 #edges = [(1,2,{'weight':2}),(1,3,{'weight':2}),(3,2,{'weight':2}),(4,2,{'weight':2}),(1,4,{'weight':2})]
 #%%
@@ -107,16 +128,13 @@ def Draw_Graph(G,save_path):#利用pyecharts绘图
     r_nodes = [{"name":"","value":0,"symbolSize":5} for i in range(G.number_of_nodes())]
     for i,name in enumerate(G.nodes()):
 	    r_nodes[i]["name"] = name
-	    r_nodes[i]["value"] = G.degree()[name]
+	    r_nodes[i]["value"] = G.degree()[name]#度数可以衡量该结点的重要程度
 	    r_nodes[i]["symbolSize"] = G.degree()[name]/10#圆的大小
     r_links = [{"source":"","target":""} for i in range(G.number_of_edges())]
-    #all = 0
     for i,(u,v) in enumerate(G.edges()):
-        all+=1
         r_links[i]["source"] = u
         r_links[i]["target"] = v
         r_links[i]["value"] = G[u][v]["weight"]
-    #print(all)
     graph = Graph()
     graph.add("",r_nodes,r_links)
     print("Graph begin to Render")
@@ -142,7 +160,7 @@ def Draw_Graph_WithNx(G,alpha,node_scale,figsize):
 #Draw_Graph_WithNx(G_sub,alpha=0.5,node_scale=5,figsize=(12,9))
 #%%                                   
 #python-louvain
-partition = community_louvain.best_partition(G) #Louvain算法划分社区,返回的是一个dict,key是文档id value是文档所属社区
+partition = community_louvain.best_partition(G) #Louvain算法划分社区,返回的是一个dict,key是文档id value是文档所属社区,可见如果边过多计算时间慢
 comm_dict = defaultdict(list)
 for doc in partition:
     comm_dict[partition[doc]].append(doc)#[{O:[node1,...,node2]}] {'社区id':"文档list"}
@@ -165,7 +183,6 @@ def Draw_Community(comm,comm_dict,isdraw = False):
     #print("community {}: {}".format(str(comm)," ".join(map(str,reversed(sorted(comm_dict[comm],key=G.degree))))))
     #print("社区元素升序:")
     #print("community {}: {}".format(str(comm)," ".join(map(str,sorted(comm_dict[comm],key=G.degree)))))
-
 #%%
 comm_dict = dict(sorted(comm_dict.items(), key=lambda d: len(d[1]),reverse = True))#按社区的元素个数排序
 rec = 0
@@ -173,6 +190,29 @@ for item in comm_dict.items():
     rec+=1
     if(rec>10):break
     Draw_Community(item[0],comm_dict,isdraw = False)
+#%%
+#保存时间帧community于一array中[{}]
+#{"community_id":社区划分后的id,"community_member":[社区成员],"member_degree":[成员的度数],"member_emb":[成员的embedding],"member_content":["社区内容"]}
+ret = []
+for item in comm_dict.items():
+    if(len(item[1])<4):#社区内数目至少要4条
+        continue
+    community_temp = {"community_id":-1,"community_member":[],"member_degree":[],"member_emb":[],"member_content":[]}
+    community_temp["community_id"] = item[0]
+    community_temp["community_member"] = item[1]
+    degree_temp = []
+    for m in item[1]:
+        degree_temp.append(G.degree()[m])
+    community_temp["member_degree"] = degree_temp
+    community_temp["member_emb"] = emb_temp[item[1]]
+    community_temp["member_content"] = keywords[item[1][:4]]
+    ret.append(community_temp)
+date = time_b.split(" ")[0].split("/")[-1]
+np.save(str(date)+".npy",np.array(ret))
+#%%
+#np.save(time_b+" "+time_e+".npy",np.array(ret))
 
+#目前来说是T=0.4 用一天的数据进行事件检测效果会好点
+#主要的问题是相同类别的数据经常会分成多个小类
 
 #%%
