e97d6c3e3f5dd7c6fcf1e0db731cbbde2bf2acba
fangzhiye <929329108@qq.com>
2019-11-29 17:03:12 +0800

    add event chain and query event

--- EventBurst_Demo/demo/urls.py
+++ EventBurst_Demo/demo/urls.py
@@ -4,5 +4,9 @@ from . import views
 urlpatterns = [
     url(r'^$',views.index,name='index'),# local host/demo 根目录是到views.index下
     #url('themeriver',views.themeriver,name="theamriver"),
-    url('test',views.test,name="test")
+    url('test',views.test,name="test"),
+    url('event_chain',views.event_chain,name="event_chain"),
+    url('event',views.event,name="event")
+    
+
 ]
--- EventBurst_Demo/demo/views.py
+++ EventBurst_Demo/demo/views.py
@@ -2,14 +2,17 @@ from django.shortcuts import render
 
 # Create your views here.
 from jinja2 import Environment, FileSystemLoader
-from pyecharts.globals import CurrentConfig
+from pyecharts.globals import CurrentConfig, SymbolType
 from django.http import HttpResponse
 
 CurrentConfig.GLOBAL_ENV = Environment(loader=FileSystemLoader("./demo/templates"))
 
 from pyecharts import options as opts
-from pyecharts.charts import Page, ThemeRiver, Bar, Map, Geo, Grid, Line, Scatter
+from pyecharts.charts import Page, ThemeRiver, Bar, Map, Geo, Grid, Line, Scatter, Timeline,WordCloud
 from pyecharts.faker import Collector, Faker
+
+from pyecharts.components import Table
+from pyecharts.options import ComponentTitleOpts
 
 def index(request):
     request.encoding='utf-8'
@@ -36,6 +39,22 @@ def get_bar():
         .set_global_opts(title_opts=opts.TitleOpts(title="Bar-基本示例", subtitle="我是副标题"))
     )
     return bar
+def get_scatter() -> Scatter:
+    #print(Faker.values())
+    #Scatter().set_global_opts()
+    scatter = (
+        Scatter()
+        .add_xaxis(['举报时间','经度','维度','举报内容'])
+        #.add_yaxis("商家A", Faker.values())
+        #.add_yaxis("商家B", Faker.values())
+        .set_global_opts(
+            #title_opts=opts.TitleOpts(title="Grid-Scatter"),
+            legend_opts=opts.LegendOpts(pos_left="20%"),
+            yaxis_opts=opts.AxisOpts(is_show=False)
+        )
+        
+    )
+    return scatter
 def get_themeriver():
     data = [
         ["2015/11/08", 10, "DQ"],
@@ -123,9 +142,13 @@ def get_themeriver():
         .add(
             ["DQ", "TY", "SS", "QG", "SY", "DD"],
             data,
-            singleaxis_opts=opts.SingleAxisOpts(type_="time", pos_bottom="10%"),
+            singleaxis_opts=opts.SingleAxisOpts(type_="time", pos_bottom="70%"),#可以通过调整坐标轴来调整图的位置
+            label_opts = opts.LabelOpts(is_show=False)
         )
-        .set_global_opts(title_opts=opts.TitleOpts(title="ThemeRiver-基本示例"))
+        .set_global_opts(title_opts=opts.TitleOpts(title="事件链"),
+                        legend_opts=opts.LegendOpts(type_ = 'scroll',pos_left="4%",pos_bottom='40%',orient='vertical'))
+                        #datazoom_opts=opts.DataZoomOpts())#图例设置
+                        
     )
     return themeriver
 
@@ -133,43 +156,37 @@ def get_line():
     line = (
         Line()
         .add_xaxis(Faker.choose())
-        .add_yaxis("商家A", Faker.values())
+        #.add_yaxis("商家A", Faker.values())
+        
         .add_yaxis("商家B", Faker.values())
         .set_global_opts(
-            title_opts=opts.TitleOpts(title="Grid-Line", pos_right="5%"),
-            legend_opts=opts.LegendOpts(pos_right="20%"),
+            title_opts=opts.TitleOpts(title="天津政府服务热线数目", pos_right="center"),
+            legend_opts=opts.LegendOpts(is_show=False),
+            #legend_opts=opts.LegendOpts(pos_top="20%"),
+            datazoom_opts=opts.DataZoomOpts(),
+            #toolbox_opts=opts.ToolboxOpts(orient='vertical',pos_right="right")
         )
     )
     
     return line
-
-def get_scatter() -> Scatter:
-    scatter = (
-        Scatter()
-        .add_xaxis(Faker.choose())
-        .add_yaxis("商家A", Faker.values())
-        .add_yaxis("商家B", Faker.values())
-        .set_global_opts(
-            title_opts=opts.TitleOpts(title="Grid-Scatter"),
-            legend_opts=opts.LegendOpts(pos_left="20%"),
-        )
-    )
-    return scatter
-
 def get_mymap(message) -> Geo:
+    pos = (117.20, 39.12)
     tianjing_map = (
          Geo()
         .add_schema(maptype="天津")
         # 加入自定义的点，格式为
-        .add_coordinate("测试点", 117.20, 39.12)
+        .add_coordinate("测试点", pos[0],pos[1])
         # 为自定义的点添加属性
-        .add("geo", [("测试点", 51)])
-        .set_series_opts(label_opts=opts.LabelOpts(is_show=True))
-        .set_global_opts(title_opts=opts.TitleOpts(title=message))
+        .add("测试点",[("测试点", 51)],type_="effectScatter")
+        #.set_series_opts()
+        .set_series_opts(label_opts=opts.LabelOpts(is_show=False),
+                         effect_opts=opts.EffectOpts(is_show=True))
+        .set_global_opts(title_opts=opts.TitleOpts(title=message,pos_right="center"),
+                        legend_opts=opts.LegendOpts(is_show=False))
     )
     return tianjing_map
 
-def test(request):
+def event_chain(request):
     '''
     grid = (
         Grid()
@@ -181,7 +198,7 @@ def test(request):
     if 'date_begin' in request.GET and request.GET['date_begin']:
         message = '你搜索的内容为: ' + request.GET['date_begin']
     else:
-        message = '你提交了空表单'
+        message = '天津市地图'
 
     page = Page(layout=Page.DraggablePageLayout)
     mymap = get_mymap(message)
@@ -198,3 +215,112 @@ def test(request):
     Page.save_resize_html("render.html", cfg_file="./demo/chart_config.json", dest="./demo/templates/my_new_charts.html")
     return render(request,"my_new_charts.html")
     #return HttpResponse(page.render_embed(template_name="simple_page_test.html"))
+
+#只考虑查看单独一个事件的内容
+def get_eventmap(message) -> Geo:
+    tianjing_map = (
+         Geo()
+        .add_schema(maptype="天津")
+        # 加入自定义的点，格式为
+        .add_coordinate("测试点", 117.20, 39.12)
+        # 为自定义的点添加属性
+        .add("测试点",[("测试点", 51)])
+        .set_series_opts(label_opts=opts.LabelOpts(is_show=False))
+        .set_global_opts(title_opts=opts.TitleOpts(title=message,pos_right="center"),legend_opts=opts.LegendOpts(is_show=False))
+    )
+    return tianjing_map
+
+def get_eventline():
+    line = (
+        Line()
+        .add_xaxis(Faker.choose())
+        #.add_yaxis("商家A", Faker.values())
+        .add_yaxis("商家B", Faker.values())
+        .set_global_opts(
+            title_opts=opts.TitleOpts(title="天津政府服务热线数目", pos_right="center"),
+            legend_opts=opts.LegendOpts(is_show=False),
+            #legend_opts=opts.LegendOpts(pos_top="20%"),
+            datazoom_opts=opts.DataZoomOpts(),
+            #toolbox_opts=opts.ToolboxOpts(orient='vertical',pos_right="right")
+        )
+    )
+    
+    return line
+def get_eventtable() -> Table:
+    table = Table()
+
+    headers = ["City name", "Area", "Population", "Annual Rainfall"]
+    rows = [
+        ["Brisbane", 5905, 1857594, 1146.4],
+        ["Adelaide", 1295, 1158259, 600.5],
+        ["Darwin", 112, 120900, 1714.7],
+        ["Hobart", 1357, 205556, 619.5],
+        ["Sydney", 2058, 4336374, 1214.8],
+        ["Melbourne", 1566, 3806092, 646.9],
+        ["Perth", 5386, 1554769, 869.4],
+        ["Sydney", 2058, 4336374, 1214.8],
+        ["Melbourne", 1566, 3806092, 646.9],
+        ["Perth", 5386, 1554769, 869.4],
+    ]
+    table.add(headers, rows).set_global_opts(
+        title_opts=ComponentTitleOpts(title="详细举报数据"),
+    )
+    return table
+
+def get_wordcloud_line()->Line:
+    tl = Timeline()
+    words = [
+    ("Sam S Club", 10000),
+    ("Macys", 6181),
+    ("Amy Schumer", 4386),
+    ("Jurassic World", 4055),
+    ("Charter Communications", 2467),
+    ("Chick Fil A", 2244),
+    ("Planet Fitness", 1868),
+    ("Pitch Perfect", 1484),
+    ("Express", 1112),
+    ("Home", 865),
+    ("Johnny Depp", 847),
+    ("Lena Dunham", 582),
+    ("Lewis Hamilton", 555),
+    ("KXAN", 550),
+    ("Mary Ellen Mark", 462),
+    ("Farrah Abraham", 366),
+    ("Rita Ora", 360),
+    ("Serena Williams", 282),
+    ("NCAA baseball tournament", 273),
+    ("Point Break", 265),
+    ]
+    for i in range(2015, 2020):
+        wordcloud = (
+            WordCloud()
+            .add("", words, word_size_range=[20, 100], shape=SymbolType.DIAMOND)
+            .set_global_opts(title_opts=opts.TitleOpts(title="事件链词云图"))
+        )
+        tl.add(wordcloud, "{}年".format(i))
+    return tl
+
+def event(request):
+    page = Page(layout=Page.DraggablePageLayout)
+    eventmap = get_eventmap(message="天津市地图")
+    eventmap.chart_id = "map_event"
+    eventline = get_eventline()
+    eventline.chart_id = "line_event"
+    wordcloudline = get_wordcloud_line()
+    wordcloudline.chart_id = "wordcloud_line"
+    eventtable = get_eventtable()
+    eventtable.chart_id = "table_event"
+    page.add(eventmap)
+    page.add(eventline)
+    page.add(wordcloudline)
+    page.add(eventtable)
+    page.render(path="event.html",template_name="simple_page_event.html")
+    Page.save_resize_html("event.html", cfg_file="./demo/chart_config_event.json", dest="./demo/templates/my_event_charts.html")
+    return render(request,"my_event_charts.html")
+    #return HttpResponse(page.render_embed(template_name="simple_page_event.html"))
+
+
+
+def test(request):
+    scatter = get_scatter()
+    return HttpResponse(scatter.render_embed())
